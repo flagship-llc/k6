e9c3713685e0caae4e3f4f967ac3018dad88693f
Ian <ian.chang@flagship.cc>
2024-04-18 14:56:40 +0900

    update example.js

--- ApiTesting/exmple.js
+++ ApiTesting/exmple.js
@@ -1,10 +1,19 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import { sleep, check } from "k6";
 export const options = {
-  vus: 10,
-  duration: "30s",
+  stages: [
+    { duration: "30s", target: 3000 },
+    { duration: "30s", target: 3000 },
+    { duration: "30s", target: 0 },
+  ],
+  thresholds: {
+    http_req_duration: ["p(95)<500"], // 95% of requests must complete below 500ms
+  },
 };
 export default function () {
-  http.get("http://test.k6.io");
+  const res = http.get("http://test.k6.io");
+  check(res, {
+    "response code was 200": (res) => res.status == 200,
+  });
   sleep(1);
 }
